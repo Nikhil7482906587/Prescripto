b2f2470e4c375f655906337a5a24e8a75f6c4887
Rounak Gupta <129253665+Rounakzone6@users.noreply.github.com>
2025-02-09 15:02:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,17 @@ import userRouter from "./routes/userRoute.js";
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
+const allowedOrigins = [process.env.FRONTEND_URL, process.env.ADMIN_URL];
 
 // middlewares
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 
 // Connect to the database
 connectDB();
