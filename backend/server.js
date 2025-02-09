6ab442e6e1a4b2b798f0837bde5b52e7a0cb25c6
Rounak Gupta <129253665+Rounakzone6@users.noreply.github.com>
2025-02-09 15:10:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,13 @@ import adminRouter from "./routes/adminRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
 import userRouter from "./routes/userRoute.js";
 
-
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
-const allowedOrigins = [process.env.FRONTEND_URL, process.env.ADMIN_URL];
 
 // middlewares
 app.use(express.json())
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
-
+app.use(cors())
 
 // Connect to the database
 connectDB();
